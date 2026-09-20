cb0a57ce056d120b6fa2798aa28970b31ce77db5
Ronak Dhakad <dhakadr932@gmail.com>
2026-09-20 22:59:19 +0530

    Time: 2122 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,7 @@
+SELECT u.name AS name,
+       COALESCE(SUM(r.distance), 0) AS travelled_distance
+FROM Users AS u
+LEFT JOIN Rides AS r
+    ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name ASC;
